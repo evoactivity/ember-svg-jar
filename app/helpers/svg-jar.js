63f250a63f34def45defc4487c56ffb7e892a7a9
djsegal <djsegal@wisc.edu>
2016-08-13 00:36:38 -0400

    Make svgJar a callable method

--- app/helpers/svg-jar.js
+++ app/helpers/svg-jar.js
@@ -3,6 +3,8 @@ import makeHelper from 'ember-svg-jar/utils/make-helper';
 import makeSVG from 'ember-svg-jar/utils/make-svg';
 import inlineAssets from '../inline-assets';
 
-export default makeHelper((assetId, svgAttrs) => (
-  htmlSafe(makeSVG(assetId, svgAttrs, inlineAssets))
-));
+export function svgJar(assetId, svgAttrs) {
+  return htmlSafe(makeSVG(assetId, svgAttrs, inlineAssets));
+}
+
+export default makeHelper(svgJar);
