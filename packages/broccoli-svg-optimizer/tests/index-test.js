b42115d97cab83cab3fdde26288d971c489a02ee
ivanvotti <ivanvotti@gmail.com>
2019-07-26 09:16:24 +0500

    Upgrade dependencies

--- packages/broccoli-svg-optimizer/tests/index-test.js
+++ packages/broccoli-svg-optimizer/tests/index-test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const SVGOptimizer = require('../');
 const fixture = require('broccoli-fixture');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const SVGOptimizer = require('../');
 
 const { expect } = chai;
 chai.use(chaiAsPromised);
@@ -14,10 +14,10 @@ describe('broccoli-svg-optimizer', () => {
   it('optimizes SVG files with persistence', () => {
     let outputNode = fixture.build(new SVGOptimizer(inputNode));
     let OPTIMIZED_CONTENT = (
-      '<svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">' +
-      '<title>SVG title</title>' +
-      '<desc>SVG description</desc>' +
-      '<path d="M7 6V0H6v6H0v1h6v6h1V7h6V6H7z"/></svg>'
+      '<svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">'
+      + '<title>SVG title</title>'
+      + '<desc>SVG description</desc>'
+      + '<path d="M7 6V0H6v6H0v1h6v6h1V7h6V6H7z"/></svg>'
     );
     return expect(outputNode).to.eventually.deep.equal({
       'test.svg': OPTIMIZED_CONTENT
@@ -28,10 +28,10 @@ describe('broccoli-svg-optimizer', () => {
     let options = { persist: false };
     let outputNode = fixture.build(new SVGOptimizer(inputNode, options));
     let OPTIMIZED_CONTENT = (
-      '<svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">' +
-      '<title>SVG title</title>' +
-      '<desc>SVG description</desc>' +
-      '<path d="M7 6V0H6v6H0v1h6v6h1V7h6V6H7z"/></svg>'
+      '<svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">'
+      + '<title>SVG title</title>'
+      + '<desc>SVG description</desc>'
+      + '<path d="M7 6V0H6v6H0v1h6v6h1V7h6V6H7z"/></svg>'
     );
 
     return expect(outputNode).to.eventually.deep.equal({
@@ -51,8 +51,8 @@ describe('broccoli-svg-optimizer', () => {
     };
     let outputNode = fixture.build(new SVGOptimizer(inputNode, options));
     let OPTIMIZED_CONTENT = (
-      '<svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">' +
-      '<path d="M7 6V0H6v6H0v1h6v6h1V7h6V6H7z"/></svg>'
+      '<svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">'
+      + '<path d="M7 6V0H6v6H0v1h6v6h1V7h6V6H7z"/></svg>'
     );
 
     return expect(outputNode).to.eventually.deep.equal({
