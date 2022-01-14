80a68add58f2034bceca06bbfa1ee06dac1d9678
Francesco Novy <francesco@fabscale.com>
2022-01-14 09:05:18 +0100

    Use normal helper instead of makeHelper util

--- app/helpers/svg-jar.js
+++ app/helpers/svg-jar.js
@@ -1,17 +1,1 @@
-import makeHelper from 'ember-svg-jar/utils/make-helper';
-import makeSVG from 'ember-svg-jar/utils/make-svg';
-
-function getInlineAsset(assetId) {
-  try {
-    /* eslint-disable global-require */
-    return require(`ember-svg-jar/inlined/${assetId}`).default;
-  } catch (err) {
-    return null;
-  }
-}
-
-export function svgJar(assetId, svgAttrs) {
-  return makeSVG(assetId, svgAttrs, getInlineAsset);
-}
-
-export default makeHelper(svgJar);
+export { default, svgJar } from 'ember-svg-jar/helpers/svg-jar';
--- tests/unit/utils/make-helper-test.js
+++
@@ -1,9 +0,0 @@
-import makeHelper from 'ember-svg-jar/utils/make-helper';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | make helper', function () {
-  test('it works', function (assert) {
-    let result = makeHelper();
-    assert.ok(result);
-  });
-});
