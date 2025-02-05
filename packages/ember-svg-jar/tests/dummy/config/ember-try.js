a6e32fb720bfac905c974dbb790d6cf2cf2a0794
betocantu93 <betocantu93@gmail.com>
2025-02-05 17:02:09 -0600

    v5.0.0...v5.8.0

--- packages/ember-svg-jar/tests/dummy/config/ember-try.js
+++ packages/ember-svg-jar/tests/dummy/config/ember-try.js
@@ -8,18 +8,18 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-4.4',
+        name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
+            'ember-source': '~4.12.0',
           },
         },
       },
       {
-        name: 'ember-lts-4.8',
+        name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
+            'ember-source': '~5.4.0',
           },
         },
       },
